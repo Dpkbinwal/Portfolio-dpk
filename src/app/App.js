a2c0255285c5717e4a32c1ff7d3fce29af4e260a
Pradeep Binwal <Pradeepbinwal1@gmail.com>
2024-02-04 12:19:37 +0530

    second

--- src/app/App.js
+++ src/app/App.js
@@ -9,22 +9,22 @@ import AppRoutes from "./routes";
 import Headermain from "../header";
 import "./App.css";
 
-// function _ScrollToTop(props) {
-//   const { pathname } = useLocation();
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, [pathname]);
-//   return props.children;
-// }
-// const ScrollToTop = withRouter(_ScrollToTop);
+function _ScrollToTop(props) {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return props.children;
+}
+const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
   return (
     <Router>
-      {/* <ScrollToTop> */}
+      <ScrollToTop>
         <Headermain />
         <AppRoutes />
-      {/* </ScrollToTop> */}
+      </ScrollToTop>
     </Router>
   );
 }
